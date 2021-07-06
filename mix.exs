3152e5cf3cfcadaa7a6919b4dd4cd86bc2aad755
greg-rychlewski <greg.rychlewski@gmail.com>
2021-07-06 17:16:36 -0400

    add :logger to extra_applications in mix.exs

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Params.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
